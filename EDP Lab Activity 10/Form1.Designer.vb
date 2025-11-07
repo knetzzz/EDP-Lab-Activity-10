c4123c3c5efef3be1bb39f6c94531f6f3af10a96
knetzzz <kennethpira2006@gmail.com>
2025-11-07 09:24:51 +0800

    I Added Design, Fix the Code and Review the Errors.

--- EDP Lab Activity 10/Form1.Designer.vb
+++ EDP Lab Activity 10/Form1.Designer.vb
@@ -22,89 +22,163 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnShowSorted = New System.Windows.Forms.Button()
+        Me.lstNumbers = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnSave
         '
-        Me.Button1.Location = New System.Drawing.Point(274, 171)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(89, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Add Number"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSave.Font = New System.Drawing.Font("Perpetua", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(135, 223)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(112, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Add Number"
+        Me.btnSave.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnShowSorted
         '
-        Me.Button2.Location = New System.Drawing.Point(369, 171)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(89, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Show Sorted"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnShowSorted.Font = New System.Drawing.Font("Perpetua", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnShowSorted.Location = New System.Drawing.Point(253, 223)
+        Me.btnShowSorted.Name = "btnShowSorted"
+        Me.btnShowSorted.Size = New System.Drawing.Size(112, 23)
+        Me.btnShowSorted.TabIndex = 1
+        Me.btnShowSorted.Text = "Show Sorted"
+        Me.btnShowSorted.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'lstNumbers
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(274, 200)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(184, 95)
-        Me.ListBox1.TabIndex = 2
+        Me.lstNumbers.Font = New System.Drawing.Font("Perpetua", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstNumbers.FormattingEnabled = True
+        Me.lstNumbers.ItemHeight = 15
+        Me.lstNumbers.Location = New System.Drawing.Point(370, 152)
+        Me.lstNumbers.Name = "lstNumbers"
+        Me.lstNumbers.Size = New System.Drawing.Size(228, 94)
+        Me.lstNumbers.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(278, 151)
+        Me.Label1.Font = New System.Drawing.Font("Perpetua", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(136, 194)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.Size = New System.Drawing.Size(108, 17)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Enter a number:"
         '
-        'TextBox1
+        'txtNumber
         '
-        Me.TextBox1.Location = New System.Drawing.Point(369, 147)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(89, 20)
-        Me.TextBox1.TabIndex = 4
+        Me.txtNumber.Location = New System.Drawing.Point(252, 193)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(112, 20)
+        Me.txtNumber.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Perpetua Titling MT", 17.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(136, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(227, 28)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Number Sorter"
         '
         'PictureBox1
         '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox1.Location = New System.Drawing.Point(261, 84)
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.PictureBox1.Location = New System.Drawing.Point(122, 118)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(211, 221)
+        Me.PictureBox1.Size = New System.Drawing.Size(488, 192)
         Me.PictureBox1.TabIndex = 5
         Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(135, 254)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(464, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "---------------------------------------------------------------------------------" &
+    "----------"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox2.Location = New System.Drawing.Point(135, 283)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(463, 18)
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox3.Location = New System.Drawing.Point(134, 127)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(463, 18)
+        Me.PictureBox3.TabIndex = 10
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.PictureBox4.Location = New System.Drawing.Point(109, 107)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(513, 217)
+        Me.PictureBox4.TabIndex = 11
+        Me.PictureBox4.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNumber)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstNumbers)
+        Me.Controls.Add(Me.btnShowSorted)
+        Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnShowSorted As Button
+    Friend WithEvents lstNumbers As ListBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents Label2 As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
 End Class
--- EDP Lab Activity 10/Form1.vb
+++ EDP Lab Activity 10/Form1.vb
@@ -1,9 +1,53 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿Imports System.IO
+Imports System.Linq
 
+Public Class Form1
+    Dim filePath As String = "numbers.txt"
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            Dim num As String = txtNumber.Text.Trim()
+
+            If Not IsNumeric(num) Then
+                MessageBox.Show("Please enter a valid number!")
+                Exit Sub
+            End If
+
+            Using writer As New StreamWriter(filePath, True)
+                writer.WriteLine(num)
+            End Using
+
+            MessageBox.Show("Number saved successfully!")
+            txtNumber.Clear()
+        Catch ex As Exception
+            MessageBox.Show("Error saving number: " & ex.Message)
+        End Try
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    Private Sub btnShowSorted_Click(sender As Object, e As EventArgs) Handles btnShowSorted.Click
+        Try
+            If Not File.Exists(filePath) Then
+                MessageBox.Show("No numbers found. Please save some numbers first.")
+                Exit Sub
+            End If
 
+            Dim lines() As String = File.ReadAllLines(filePath)
+            Dim numbers As List(Of Integer) = lines.Where(Function(x) IsNumeric(x)).Select(Function(x) CInt(x)).ToList()
+
+            If numbers.Count = 0 Then
+                MessageBox.Show("No valid numbers found in the file.")
+                Exit Sub
+            End If
+
+            Dim sortedNumbers = numbers.OrderBy(Function(n) n)
+            lstNumbers.Items.Clear()
+            For Each n In sortedNumbers
+                lstNumbers.Items.Add(n)
+            Next
+
+        Catch ex As Exception
+            MessageBox.Show("Error reading or sorting numbers: " & ex.Message)
+        End Try
     End Sub
 End Class
+
